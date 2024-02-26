a5b227eb497792e77c995137790e89a392fd3388
Vladimir Rachkin <vova@kpnn.ru>
2024-02-27 01:39:29 +0300

    Add aces

--- src/Color.cpp
+++ src/Color.cpp
@@ -3,12 +3,24 @@
 //
 
 #include "Color.h"
+#include <glm/vec3.hpp>
 
 Color operator*(double d, Color other) {
     return {d * other.r, d * other.g, d * other.b};
 }
 
+double aces_tonemap(double x)
+{
+    const double a = 2.51f;
+    const double b = 0.03f;
+    const double c = 2.43f;
+    const double d = 0.59f;
+    const double e = 0.14f;
+    return (x*(a*x+b))/(x*(c*x+d)+e);
+}
+
 u_int8_t b2f(double c) {
+    c = aces_tonemap(c);
     c = pow(c, 1 / 2.2);
-    return fmin(round(c * 255), 255.0);
+    return fmax(fmin(round(c * 255), 255.0), 0.0);
 }
